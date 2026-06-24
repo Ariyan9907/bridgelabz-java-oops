cee9e230e738e7fdaf86254ab636c0f05ad5511c
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-24 22:43:41 +0530

    [Ariyan] methods added in stock portafolio

--- src/com/src/StockPortfolio.java
+++ src/com/src/StockPortfolio.java
@@ -1,4 +1,31 @@
 package com.src;
 
+import java.util.ArrayList;
+
 public class StockPortfolio {
+
+    private ArrayList<Stock> stocks=new ArrayList<Stock>();
+
+    public void addStock(Stock stock){
+        stocks.add(stock);
+    }
+
+    public int calculateTotalValue(){
+        int total=0;
+        for(Stock stock:stocks){
+           total+=stock.calculateStockValue();
+        }
+
+        return total;
+    }
+
+    public void printReport(){
+        for(Stock stock:stocks){
+            System.out.println("Stock Name:"+stock.getStockName());
+            System.out.println("No Of Shares:"+stock.getNumberOfShares());
+            System.out.println("Shares Price:"+stock.calculateStockValue());
+            System.out.println("Total Value"+calculateTotalValue());
+        }
+    }
+
 }
