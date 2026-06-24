740c4cbcde2a35c197002bc264d005d28b466fa2
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2026-06-24 14:42:28 +0530

    [Ariyan]:Created contact object and added in addressbook

--- src/com/src/AddressBook.java
+++ src/com/src/AddressBook.java
@@ -1,4 +1,12 @@
 package com.src;
 
+import java.util.ArrayList;
+
 public class AddressBook {
+    ArrayList<Contact> contacts = new ArrayList<>();
+
+    public void addContact(Contact contact){
+        contacts.add(contact);
+    }
+
 }
--- src/com/src/AddressBookMain.java
+++ src/com/src/AddressBookMain.java
@@ -1,7 +1,35 @@
 package com.src;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to adreess book ");
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String firstName=sc.nextLine();
+        System.out.println("Enter your last name:");
+        String lastName=sc.nextLine();
+        System.out.println("Enter your Address:");
+        String address=sc.nextLine();
+        System.out.println("Enter your city:");
+        String city=sc.nextLine();
+        System.out.println("Enter your state:");
+        String state=sc.nextLine();
+        System.out.println("Enter your zip:");
+        String zip = sc.nextLine();
+        System.out.println("Enter your phone number:");
+        String phone=sc.nextLine();
+        System.out.println("Enter your email:");
+        String email=sc.nextLine();
+
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phone, email);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact(contact);
+        System.out.println("Contact Added Successfully");
+
+
+
     }
 }
